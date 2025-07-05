461f1cfa5fd3fad12f2830812ff663290ce395d9
Daniel <dani31jijo@gmail.com>
2025-07-06 00:52:01 +0530

    fix codespaces

--- webapp-ui/vite.config.ts
+++ webapp-ui/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'process'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://server:5000',
+        target: process.env.VITE_BACKEND_URL || 'http://localhost:5000',
         changeOrigin: true,
       },
     },
